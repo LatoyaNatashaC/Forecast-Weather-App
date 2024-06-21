389503af6cbb415d908d56e60feaacda9d0e5510
LatoyaNatashaC <Lnatashacampbell@gmail.com>
2024-06-21 15:13:27 -0700

    Forecast is now displaying

--- src/index.js
+++ src/index.js
@@ -64,6 +64,12 @@ function search(event) {
 
   searchCity(searchInput.value);
 }
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[date.getDay()];
+}
 
 function getForecast(city) {
   let apiKey = "4f3b0tf3219b4c7758082d0o48eabbbe";
@@ -77,23 +83,27 @@ function displayForecast(response) {
   let forecastElement = document.querySelector("#forecast");
   let forecastHtml = "";
 
-  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
-
-  days.forEach(function (day) {
-    forecastHtml =
-      forecastHtml +
-      `
+  response.data.daily.forEach(function (day, index) {
+    if (index < 5) {
+      forecastHtml =
+        forecastHtml +
+        `
  <div class= "weather-forecast-day">
-  <div class="weather-forecast-date">${day}</div>
-  <div class="weather-forecast-icon">🌤</div>
+  <div class="weather-forecast-date">${formatDay(day.time)}</div>
+  <div >
+  <img src= "${day.condition.icon_url}" class="weather-forecast-icon"/>
+  </div>
   <div class="Weather-forecast-temperatures">
   <span class="weather-forecast-temperature-max">
-  <strong>79º</strong>
+  <strong>${Math.round(day.temperature.maximum)}º</strong>
   </span>
-  <span class="weather-forecast-temperature-min"> 55º</span>
+  <span class="weather-forecast-temperature-min">${Math.round(
+    day.temperature.minimum
+  )}º</span>
   </div>
   </div>
 `;
+    }
   });
 
   forecastElement.innerHTML = forecastHtml;
